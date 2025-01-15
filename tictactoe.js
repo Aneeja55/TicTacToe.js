18023541455725b72275b0e822243f73e45abdf6
Aneeja55 <aneeja5505@gmail.com>
2025-01-15 16:23:43 +0500

    made Changes

--- tictactoe.js
+++ tictactoe.js
@@ -5,17 +5,17 @@ let gameActive= true;
 
 function printBoard(){
     console.log(`
-        ${gameBoard[0]  |  gameBoard[1]  |  gameBoard[2]}
-        ___________________
-        ${gameBoard[3]  |  gameBoard[4]  |  gameBoard[5]}
-        ___________________
-        ${gameBoard[6]  |  gameBoard[7]  |  gameBoard[8]}
-        `);
+        ${gameBoard[0]} | ${gameBoard[1]} | ${gameBoard[2]}
+        ---------
+        ${gameBoard[3]} | ${gameBoard[4]} | ${gameBoard[5]}
+        ---------
+        ${gameBoard[6]} | ${gameBoard[7]} | ${gameBoard[8]}
+    `);
 }
 
-function handleMove(pos){
-    if(gameBoard[pos]==' '){
-        gameBoard[pos]=currentPlayer;
+function handleMove(position){
+    if(gameBoard[position]==' '){
+        gameBoard[position]=currentPlayer;
     }else{
         console.log("Position already Occupied!Choose another");
         return false;
@@ -29,19 +29,22 @@ function handleMove(pos){
     }
     
     if (gameBoard.every(cell => cell!==' ')) {
+        printBoard();
         console.log("It is a Draw!!");
         gameActive=false;
         return true;
     }
 
-    currentPlayer= currentPlayer==="X"?"O":"X";
+    currentPlayer= currentPlayer==='X'?'O':'X';
     return true;
 }
 
 function checkWin(){
-    const conditions=[[0,1,2],[3,4,5],[6,7,8],
-    [0,3,6],[1,4,7],[2,5,8],
-    [0,4,8],[2,4,6]];
+    const conditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
     return conditions.some(condition => {
         const [a,b,c]= condition;
         return gameBoard[a]===currentPlayer && gameBoard[b]===currentPlayer && gameBoard[c]===currentPlayer;
@@ -50,10 +53,11 @@ function checkWin(){
 
 while (gameActive){
     printBoard();
-    const position=prompt(`Player ${currentPlayer}, Enter Your move(0-8): `);
+    const p=prompt(`Player ${currentPlayer}, Enter Your move(0-8): `);
+    const position=parseInt(p);
 
     if(position>=0&&position<=8){
-        handleMove(parseInt(position));
+        handleMove(position);
     }else{
         console.log("Invalid position, enter again");
     }
